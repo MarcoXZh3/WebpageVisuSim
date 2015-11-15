8d3418b6c3b34a823044a5340475ad2de62e89c0
MarcoXZh <zxu3@ualberta.ca>
2015-11-14 23:51:40 -0700

    ncds.py: NCD added

---
+++ ncd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# https://github.com/DavyLandman/ncd
+#./ncds.py databases/Subset01/*.png > databases/re.txt
+
+max=2
+output=databases/ncd-result.txt;
+
+for ((i = 1; i <= $max; i++)); do
+    path=$(printf "databases/Subset%02d/*.png" "$i")
+    output=$(printf "databases/ncd-results%02d.txt" "$i")
+    echo Subset -- $i
+    ./ncds.py $path > $output
+    echo
+done
+
